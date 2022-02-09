3ae820d02d457d02e0aa0b5012418dc4bcbb4e21
Sam Robertson <robertsonnew@gmail.com>
2022-02-08 18:31:45 -0700

    Hey boiizzzz

--- Mission6/Controllers/HomeController.cs
+++ Mission6/Controllers/HomeController.cs
@@ -35,3 +35,7 @@ namespace Mission6.Controllers
         }
     }
 }
+
+// Josh is un hozier
+
+// I'll worship like a dog at the shrine of your lies. I'll show you my sins and you can sharpen your knife. Offer me that debtless somthing. Good Gosh let me give you my life
