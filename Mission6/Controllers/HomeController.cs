5f4e4d410fc2aa48e8c897fff586c8838f8180a0
Sam Robertson <robertsonnew@gmail.com>
2022-02-08 21:15:48 -0700

    Deleted Error view and added Task Name

--- Mission6/Controllers/HomeController.cs
+++ Mission6/Controllers/HomeController.cs
@@ -27,12 +27,6 @@ namespace Mission6.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
 
